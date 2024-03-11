5b1c27151f17adf9431f316936e5e31a69c87fd5
SimoneCerri <iosonosimone@hotmail.it>
2024-03-11 17:53:28 +0100

    some tests

--- vue-hello/app.js
+++ vue-hello/app.js
@@ -10,11 +10,24 @@ createApp
             message: "E' stato più complicato capire perchè non funzionava che scrivere questo messaggio. Ricordarsi che la formattazione 'brutta' è obbligatoria per il 'return'." ,
             link: "./img/brain.jpg",
             number: 2,
-            count : 0
+            count : 0,
+            newItem: "",
+            flavours: [],
         }
-    },/* 
-    doThis()
+    },
+    methods:
     {
-        count++
-    } */
+        tryThis(e)
+        {
+            alert("DAJE CAZZO")
+        },
+        sayHello(my_message)
+        {
+            alert(my_message)
+        },
+        doSomething(e)
+        {
+            alert("Is a try, ups")
+        }
+    }
 }).mount("#app");
