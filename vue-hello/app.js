f3b8ede015cb2edb335898bdae5ad050d4e16b88
SimoneCerri <iosonosimone@hotmail.it>
2024-03-11 15:18:19 +0100

    done with my usual LUCK

--- vue-hello/app.js
+++ vue-hello/app.js
@@ -1,9 +1,15 @@
-createApp({
-    data(){
-        return {
-            message: "prova"
+console.log("ciah");
+
+const { createApp } = Vue
+
+createApp
+({
+    data()
+    {
+        return{
+            message: "E' stato più complicato capire perchè non funzionava che scrivere questo messaggio. Ricordarsi che la formattazione 'brutta' è obbligatoria per il 'return'." ,
+            link: "./img/brain.jpg"
         }
     }
 }).mount("#app");
 
-console.log("ciah");
